1b63a3fbdcef93d5e9d02d31faade2f5a4546439
mnjprk <165750909+mnjprk@users.noreply.github.com>
2025-11-10 20:03:12 +0000

    두번째

---
+++ Class_1/2439번/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(){
+	int N;
+	cin>>N;
+	
+	for(int i = N; i > 0; i--){
+		for(int  j = 0; j < i - 1; j ++) cout<<" ";
+		for(int k = 0; k < N - i + 1; k++) cout<<"*";
+		cout<<endl;
+	}
+}
